fb3a4c096cc3fdc6e5bb8e8d937c7198d5bfa852
Kanishka Ganguly <kanishkaganguly2002@gmail.com>
2014-03-01 12:43:02 +0530

    UI Changes

--- scripts/player.js
+++ scripts/player.js
@@ -2,17 +2,24 @@
 		var song_list = new Array();
 		var audioplayer = document.getElementById("audio");
 		var nowplaying = document.getElementById("playing");
+		var playtime = document.getElementById("time");
 		var song_counter = 0;
 		var total_songs;
 		function song_loaded(){
 			("LOADED");
+			setupSeek();
 			audioplayer.play();
 		}
 
 		function volchange() {
 			var audio = $("#audio");
 			audioplayer.volume = (document.getElementById('rangeinput').value / 100);
-			document.getElementById("rangevalue").value =  document.getElementById('rangeinput').value;
+			document.getElementById("rangevalue").value = document.getElementById('rangeinput').value;
+		}
+		function seekchange() {
+			var audio = $("#audio");
+			audioplayer.currentTime = (document.getElementById('seekrangeinput').value);
+			document.getElementById("seekrangevalue").value = document.getElementById('seekrangeinput').value;
 		}
 
 		$("#clearBtn").click(function(){
@@ -25,7 +32,7 @@
 		});	
 
 		$("#nextBtn").click(function(){
-			if(song_counter == total_songs){
+			if(song_counter == playlist.length - 1){
 				song_counter = 0;
 			}else{
 				song_counter++;
@@ -36,12 +43,11 @@
 			$("#mp3source").attr("src", playlist[song_counter]);
 			audio[0].pause();
 			audio[0].load();	
-			//audio[0].play();
 		});	
 
 		$("#prevBtn").click(function(){
 			if(song_counter == 0){
-				
+				song_counter = playlist.length-1;
 			}else{
 				song_counter--;
 			}
@@ -51,7 +57,6 @@
 			$("#mp3source").attr("src", playlist[song_counter]);
 			audio[0].pause();
 			audio[0].load();
-			//audio[0].play();
 		});	
 
 		$("#startBtn").click(function(){
@@ -82,7 +87,7 @@
 		});
 
 		function next_song(){
-			if(song_counter == total_songs){
+			if(song_counter == playlist.length-1){
 				song_counter = 0;
 			}else{
 				song_counter++;
@@ -117,7 +122,46 @@
 				reader.readAsDataURL(f);
 			}
 		}
+
+		function setupSeek(){
+			document.getElementById("seekrangeinput").value = 0;
+			document.getElementById("seekrangeinput").min = 0;
+	        document.getElementById("seekrangeinput").max = audioplayer.duration;
+		}
+
 		document.getElementById('input').addEventListener('change', handleFileSelect, false);
 		document.getElementById("rangeinput").addEventListener('change',volchange,false);
+		document.getElementById("seekrangeinput").addEventListener('change',seekchange,false);
+
 		audioplayer.addEventListener('ended', next_song, false);
 		audioplayer.addEventListener('play', change_play, false);
+
+		audioplayer.addEventListener('timeupdate',function (){
+			var currentTime = audioplayer.currentTime;
+			var duration = audioplayer.duration;
+			var dsec= new Number();
+			var dmin= new Number();
+			var csec= new Number();
+			var cmin= new Number();
+
+			dsec = Math.floor( duration );    
+			dmin = Math.floor( dsec / 60 );
+			dmin = dmin >= 10 ? dmin : '0' + dmin;    
+			dsec = Math.floor( dsec % 60 );
+			dsec = dsec >= 10 ? dsec : '0' + dsec;
+
+			csec = Math.floor( currentTime );    
+			cmin = Math.floor( csec / 60 );
+			cmin = cmin >= 10 ? cmin : '0' + cmin;    
+			csec = Math.floor( csec % 60 );
+			csec = csec >= 10 ? csec : '0' + csec;
+
+			time.innerHTML = "(" + (cmin + ":"+ csec) + " / " + (dmin + ":" + dsec) + ")";
+			document.getElementById("seekrangevalue").value = (cmin + ":"+ csec);
+			document.getElementById("seekrangeinput").value = audioplayer.currentTime;
+		});
+
+		$("#playlistToggle").click(function(){
+			$( "#play_list" ).slideToggle( "slow", function() {
+    		});
+		});
