d67271e1b5b312fb4fc170bcc102379df3ccc67c
Kanishka Ganguly <kanishkaganguly2002@gmail.com>
2014-03-05 12:55:58 +0530

    Added Scroll for Volume - Slightly buggy

--- scripts/player.js
+++ scripts/player.js
@@ -201,97 +201,120 @@
 					}
 				}
 			}
+			var audio = $("#audio");
+			var myLi = document.getElementById('play_list').getElementsByTagName('li');
+			for(var i=0; i< (myLi.length);i++){
+				myLi[i].style.background="none";
+				myLi[i].style.color="white";
+			}
+			$("li").eq(song_counter).css("background", "white");
+			$("li").eq(song_counter).css("color", "black");
+			$("li").eq(song_counter).get(0).scrollIntoView();
+			nowplaying.innerHTML = song_list[song_counter];
+			$("#mp3source").attr("src", playlist[song_counter]);
+			audio[0].pause();
+			audio[0].load();
+		}
+
+		function handleFileSelect(evt) {
+			var files = evt.target.files;
+			for(var i=0,f;f=files[i];i++){
+				var reader = new FileReader();
+				var playList = document.getElementById('play_list');
+				reader.onload = (function(theFile) {
+					return function(e) {
+						playlist[add_counter] = e.target.result;
+						song_list[add_counter] = theFile.name;
+						add_counter++;
+						var newLI = document.createElement("li");
+						newLI.id = add_counter;
+						var newContent = document.createTextNode(theFile.name);
+						newLI.appendChild(newContent);
+						playList.appendChild(newLI);
+					};
+				})(f);
+				reader.readAsDataURL(f);
+			}
+		}
+
+		function setupSeek(){
+			document.getElementById("seekrangeinput").value = 0;
+			document.getElementById("seekrangeinput").min = 0;
+			document.getElementById("seekrangeinput").max = audioplayer.duration;
+		}
+
+		document.getElementById('input').addEventListener('change', handleFileSelect, false);
+		document.getElementById("rangeinput").addEventListener('change',volchange,false);
+		document.getElementById("seekrangeinput").addEventListener('change',seekchange,false);
+		audioplayer.addEventListener('ended', next_song, false);
+		audioplayer.addEventListener('play', change_play, false);
+
+
+		$(window).bind('mousewheel', function(e) {
+			if(e.originalEvent.wheelDelta / 20 > 0) {
 				var audio = $("#audio");
-				var myLi = document.getElementById('play_list').getElementsByTagName('li');
-				for(var i=0; i< (myLi.length);i++){
-					myLi[i].style.background="none";
-					myLi[i].style.color="white";
-				}
-				$("li").eq(song_counter).css("background", "white");
-				$("li").eq(song_counter).css("color", "black");
-				$("li").eq(song_counter).get(0).scrollIntoView();
-				nowplaying.innerHTML = song_list[song_counter];
-				$("#mp3source").attr("src", playlist[song_counter]);
-				audio[0].pause();
-				audio[0].load();
-			}
-
-			function handleFileSelect(evt) {
-				var files = evt.target.files;
-				for(var i=0,f;f=files[i];i++){
-					var reader = new FileReader();
-					var playList = document.getElementById('play_list');
-					reader.onload = (function(theFile) {
-						return function(e) {
-							playlist[add_counter] = e.target.result;
-							song_list[add_counter] = theFile.name;
-							add_counter++;
-							var newLI = document.createElement("li");
-							newLI.id = add_counter;
-							var newContent = document.createTextNode(theFile.name);
-							newLI.appendChild(newContent);
-							playList.appendChild(newLI);
-						};
-					})(f);
-					reader.readAsDataURL(f);
+				if(audioplayer.volume == 1){
+					audioplayer.volume;	
+				}else{
+					audioplayer.volume += 2/100;
+					document.getElementById('rangeinput').value = (audioplayer.volume * 100);
+					document.getElementById("rangevalue").value = document.getElementById('rangeinput').value;
+				}			
+			} else {
+				var audio = $("#audio");
+				if(audioplayer.volume == 0){
+					audioplayer.volume;	
+				}else{
+					audioplayer.volume -= 2/100;
+					document.getElementById('rangeinput').value = (audioplayer.volume * 100);
+					document.getElementById("rangevalue").value = document.getElementById('rangeinput').value;
 				}
 			}
+		});
 
-			function setupSeek(){
-				document.getElementById("seekrangeinput").value = 0;
-				document.getElementById("seekrangeinput").min = 0;
-				document.getElementById("seekrangeinput").max = audioplayer.duration;
+		audioplayer.addEventListener('timeupdate',function (){
+			var currentTime = audioplayer.currentTime;
+			var duration = audioplayer.duration;
+			var dsec= new Number();
+			var dmin= new Number();
+			var csec= new Number();
+			var cmin= new Number();
+
+			dsec = Math.floor( duration );    
+			dmin = Math.floor( dsec / 60 );
+			dmin = dmin >= 10 ? dmin : '0' + dmin;    
+			dsec = Math.floor( dsec % 60 );
+			dsec = dsec >= 10 ? dsec : '0' + dsec;
+
+			csec = Math.floor( currentTime );    
+			cmin = Math.floor( csec / 60 );
+			cmin = cmin >= 10 ? cmin : '0' + cmin;    
+			csec = Math.floor( csec % 60 );
+			csec = csec >= 10 ? csec : '0' + csec;
+
+			time.innerHTML = "(" + (cmin + ":"+ csec) + " / " + (dmin + ":" + dsec) + ")";
+			document.getElementById("seekrangevalue").value = (cmin + ":"+ csec);
+			document.getElementById("seekrangeinput").value = audioplayer.currentTime;
+		});
+
+		$("#playlistToggle").click(function(){
+			$( "#play_list" ).slideToggle( "slow", function() {
+			});
+		});
+
+		function randomInt(min,max)
+		{
+			return Math.floor(Math.random()*(max-(min+1))+(min+1));
+		}
+
+
+		$(document).keypress(function(event){
+			var keycode = (event.keyCode ? event.keyCode : event.which);
+			if(keycode == '32'){
+				playBtn_DOM.click();
+			}else if(keycode == '97'){
+				prevBtn_DOM.click();
+			}else if(keycode == '100'){
+				nextBtn_DOM.click();
 			}
-
-			document.getElementById('input').addEventListener('change', handleFileSelect, false);
-			document.getElementById("rangeinput").addEventListener('change',volchange,false);
-			document.getElementById("seekrangeinput").addEventListener('change',seekchange,false);
-			audioplayer.addEventListener('ended', next_song, false);
-			audioplayer.addEventListener('play', change_play, false);
-
-			audioplayer.addEventListener('timeupdate',function (){
-				var currentTime = audioplayer.currentTime;
-				var duration = audioplayer.duration;
-				var dsec= new Number();
-				var dmin= new Number();
-				var csec= new Number();
-				var cmin= new Number();
-
-				dsec = Math.floor( duration );    
-				dmin = Math.floor( dsec / 60 );
-				dmin = dmin >= 10 ? dmin : '0' + dmin;    
-				dsec = Math.floor( dsec % 60 );
-				dsec = dsec >= 10 ? dsec : '0' + dsec;
-
-				csec = Math.floor( currentTime );    
-				cmin = Math.floor( csec / 60 );
-				cmin = cmin >= 10 ? cmin : '0' + cmin;    
-				csec = Math.floor( csec % 60 );
-				csec = csec >= 10 ? csec : '0' + csec;
-
-				time.innerHTML = "(" + (cmin + ":"+ csec) + " / " + (dmin + ":" + dsec) + ")";
-				document.getElementById("seekrangevalue").value = (cmin + ":"+ csec);
-				document.getElementById("seekrangeinput").value = audioplayer.currentTime;
-			});
-
-			$("#playlistToggle").click(function(){
-				$( "#play_list" ).slideToggle( "slow", function() {
-				});
-			});
-
-			function randomInt(min,max)
-			{
-				return Math.floor(Math.random()*(max-(min+1))+(min+1));
-			}
-
-
-			$(document).keypress(function(event){
-				var keycode = (event.keyCode ? event.keyCode : event.which);
-				if(keycode == '32'){
-					playBtn_DOM.click();
-				}else if(keycode == '97'){
-					prevBtn_DOM.click();
-				}else if(keycode == '100'){
-					nextBtn_DOM.click();
-				}
-			});
+		});
